a8aa18f904f0ae53b7a6cf72b9b31564176ba5e6
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-04-28 03:40:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# fasm-choco
+
+Source code for **fasm** chocolatey package.
+
+### License
+
+This repo is released under the [MIT](https://github.com/thechampagne/fasm-choco/blob/main/LICENSE).
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$packageName= 'fasm'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'https://flatassembler.net/fasmw17330.zip'
+  
+  checksum      = '0570454A4F93BAF68BAB4AD2A7A5E25EB86F1DF62FA57DD6942842426C94286A'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
